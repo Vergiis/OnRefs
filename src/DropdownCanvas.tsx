54ed761649263c669cc17de7a0140ee34912ce4f
Vergiis <56695859+Vergiis@users.noreply.github.com>
2023-09-21 21:07:07 +0200

    1 file modified

--- src/DropdownCanvas.tsx
+++ src/DropdownCanvas.tsx
@@ -1,15 +1,31 @@
 import Dropdown from 'react-bootstrap/Dropdown';
 
-export function DropdownCanvas(){
-    return(
-        <Dropdown className='list-inline-item'>
-            <Dropdown.Toggle variant="info" id="canvasDropdownBtn" className='dropdownBtn navbar-text' aria-haspopup="true" aria-expanded="false">
-                Unsaved*
-            </Dropdown.Toggle>
+export function DropdownCanvas() {
+  return (
+    <Dropdown className="list-inline-item">
+      <Dropdown.Toggle
+        variant="info"
+        id="canvasDropdownBtn"
+        className="dropdownBtn navbar-text"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        uUnsaved*
+      </Dropdown.Toggle>
 
-            <Dropdown.Menu className='scrollable-menu' aria-labelledby="dropdownMenuButton" id="canvasesDropdown">
-                <Dropdown.Item href="/load?canvas=" id='canvasItem' className='dropdown-item'>Action</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
-    )
+      <Dropdown.Menu
+        className="scrollable-menu"
+        aria-labelledby="dropdownMenuButton"
+        id="canvasesDropdown"
+      >
+        <Dropdown.Item
+          href="/load?canvas="
+          id="canvasItem"
+          className="dropdown-item"
+        >
+          Action
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
 }
