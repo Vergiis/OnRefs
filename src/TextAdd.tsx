e3309dbec27255a5b5a2cba8bd76033a273db0d0
Vergiis <56695859+Vergiis@users.noreply.github.com>
2023-11-09 15:04:21 +0100

    2 files modified

--- src/TextAdd.tsx
+++ src/TextAdd.tsx
@@ -1,11 +1,28 @@
 import { Form } from 'react-bootstrap';
+import { BiRightArrow } from 'react-icons/bi';
 import { MdOutlineTextFields } from 'react-icons/md';
 
 export function TextAdd() {
   return (
-    <Form className="addTextInput">
+    <Form className="addText">
+      <Form.Group className="addOptionWindow">
+        <Form.Control type="text" placeholder="Text" className="addTextInput" />
+        <Form.Control
+          type="number"
+          min="0"
+          placeholder="Size"
+          className="addTextInput"
+        />
+        <Form.Control
+          type="color"
+          placeholder="Size"
+          className="addTextInput"
+          id="addTextColor"
+        />
+      </Form.Group>
       <Form.Group>
         <Form.Label id="addTextButton">
+          <BiRightArrow />
           <MdOutlineTextFields />
         </Form.Label>
       </Form.Group>
