dfb7141c621f0b4b1d3a2e117a14a78553e9e356
Vergiis <56695859+Vergiis@users.noreply.github.com>
2023-12-08 14:04:12 +0100

    2 files modified

--- src/CanvasSettings.tsx
+++ src/CanvasSettings.tsx
@@ -1,7 +1,14 @@
+import { useRef } from 'react';
 import { Form } from 'react-bootstrap';
 import { FaGear } from 'react-icons/fa6';
 
 export function CanvasSettings() {
+  const colorRef: any = useRef(null);
+
+  const colorChange = () => {
+    console.log(colorRef.current.value);
+  };
+
   return (
     <Form className="canvasSettings">
       <Form.Group>
@@ -9,10 +16,17 @@ export function CanvasSettings() {
           <FaGear />
         </Form.Label>
       </Form.Group>
-      <Form.Group
-        className="canvasSettingsWindow"
-        id="canvasSettingsToggle"
-      ></Form.Group>
+      <Form.Group className="canvasSettingsWindow" id="canvasSettingsToggle">
+        <Form.Group className="addTextInput" id="addTextColorGroup">
+          <Form.Label>Selection Color :</Form.Label>
+          <Form.Control
+            type="color"
+            id="addTextColor"
+            ref={colorRef}
+            onChange={colorChange}
+          />
+        </Form.Group>
+      </Form.Group>
     </Form>
   );
 }
