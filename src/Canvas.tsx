7a403389b2ed183fd2e0dd38e186943ad4006654
Vergiis <56695859+Vergiis@users.noreply.github.com>
2023-12-08 09:56:30 +0100

    3 files modified

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -2,7 +2,6 @@ import { useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useCookies } from 'react-cookie';
 import $ from 'jquery';
-
 let canvas: any;
 let ctx: any;
 
