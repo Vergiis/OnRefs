b887321ce64ad14d313dd297c28dac33da7cf50d
Vergiis <56695859+Vergiis@users.noreply.github.com>
2023-12-08 13:31:59 +0100

    2 files modified

--- src/CanvasSettings.tsx
+++ src/CanvasSettings.tsx
@@ -1,3 +1,26 @@
+import { Form } from 'react-bootstrap';
+import { MdAddCircle, MdOutlineTextFields } from 'react-icons/md';
+
 export function CanvasSettings() {
-  return <></>;
+  return (
+    <Form className="canvasSettings">
+      <Form.Group className="addOptionWindow" id="canvasSettingsToggle">
+        <Form.Control
+          type="text"
+          placeholder="Text"
+          className="addTextInput"
+          id="addTextValue"
+        />
+
+        <Form.Group className="addTextInput" id="addTextButtonsGroup">
+          <MdAddCircle className="addTextIcon" />
+        </Form.Group>
+      </Form.Group>
+      <Form.Group>
+        <Form.Label id="addTextButton">
+          <MdOutlineTextFields />
+        </Form.Label>
+      </Form.Group>
+    </Form>
+  );
 }
