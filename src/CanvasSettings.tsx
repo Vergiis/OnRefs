f412b523b52140ec72e53b7e0d268ad48afcdb7f
Vergiis <56695859+Vergiis@users.noreply.github.com>
2023-12-08 14:10:12 +0100

    1 file modified

--- src/CanvasSettings.tsx
+++ src/CanvasSettings.tsx
@@ -1,9 +1,20 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Form } from 'react-bootstrap';
+import { useCookies } from 'react-cookie';
 import { FaGear } from 'react-icons/fa6';
 
 export function CanvasSettings() {
   const colorRef: any = useRef(null);
+
+  const [cookies, setCookie] = useCookies(['canvasSettings']);
+  useEffect(() => {
+    if (cookies.canvasSettings == null) {
+      setCookie('canvasSettings', { sColor: '#FF0000', stype: 'Normal' });
+      colorRef.current.value = '#FF0000';
+    } else {
+      colorRef.current.value = cookies.canvasSettings.sColor;
+    }
+  }, []);
 
   const colorChange = () => {
     console.log(colorRef.current.value);
